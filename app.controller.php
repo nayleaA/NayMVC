006c0033e5e5714a1c9fb1b421238b3b24a70fce
nayleaA <naygdz306@gmail.com>
2023-11-13 09:42:06 -0600

    regresando cambio

--- app.controller.php
+++ app.controller.php
@@ -2,19 +2,19 @@
 switch ($path_components[1]) {
     case '':
     case 'pracjs':
-        require_once('app/pracjs/controller/pracjs.controller.php');
+        require_once('./app/pracjs/controller/pracjs.controller.php');
         break;
     
     case 'registro':
-        require_once('app/registro/controller/registro.controller.php');
+        require_once('./app/registro/controller/registro.controller.php');
         break;
 
     case 'tareas':
-        require_once("app/tareas/tareas.controller.php");
+        require_once("./app/tareas/tareas.controller.php");
         break;
     
     case 'app-paises':
-        require_once("app/paises/paises.controller.php");
+        require_once("./app/paises/paises.controller.php");
         break;
         
     default:
